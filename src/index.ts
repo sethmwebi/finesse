d966c6d8358a04ac59ad867fae3dc8471fa855bc
Seth Mwebi <sethmwebi27@gmail.com>
2023-04-07 12:26:28 +0300

    create graphql schema & resolver

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,46 @@
+require("dotenv").config()
 import { ApolloServer } from "@apollo/server"
+import { Client } from "@apperate/iexjs"
 import { startStandaloneServer } from "@apollo/server/standalone"
 import { gql } from "graphql-tag"
+import GraphQLJSON, { GraphQLJSONObject } from 'graphql-type-json';
 
 const typeDefs = gql`
+  scalar JSON
   type Query {
     hello: String!
   }
+
+  type Mutation {
+    quote(symbol: String!): QuoteResult!
+  }
+
+  type QuoteResult {
+    change: Float!
+    changePercentage: Float!
+    companyName: String!
+    peRatio: Float!
+    symbol: String!
+    delayedPrice: Float
+    previousClose: Float!
+  }
 `
 
+const client = new Client({api_token: process.env.IEX_API_TOKEN, version: "v1"});
+
+const getQuote = (symbol: string) => {
+  return client.quote({ symbol: "AAPL"})
+}
+
 const resolvers = {
+  JSON: GraphQLJSON,
   Query: {
     hello: () => "world"
+  },
+  Mutation: {
+    quote: (_: any, {symbol}: {symbol: string}) => {
+      return getQuote(symbol)
+    }
   }
 }
 const server = new ApolloServer({
